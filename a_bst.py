181505a7eb6fab35ecfbb1762f8eeb1e9c8a277b
pflores17 <pflore17@calpoly.edu>
2023-06-05 12:46:22 -0700

    implement

--- a_bst.py
+++ a_bst.py
@@ -24,11 +24,16 @@ class Tree:
 
 # NOT given to students
 def initialize() -> Tree:
+    return Tree()
     return None # TODO
 
 
 # NOT given to students
 def isEmpty(tree: Tree) -> bool:
+    if tree.root == None:
+        return True
+    else:
+        return False
     return None # TODO
 
 
@@ -56,47 +61,45 @@ def preorder_traversal(tree: Node, level:int=0):
 def inorder_traversal(tree: Node, level:int=0):
     if level == 0:
         print('in order traversal')
-    if False: # TODO
-        # TODO
-        return
+    if tree != None:
+        inorder_traversal(tree.left, level +1)
+        print(f' level = {level:^3d} : value = {tree.value}')
+        inorder_traversal(tree.right, level +1)
 
 
 # NOT given to the students
 def postorder_traversal(tree: Node, level:int=0):
     if level == 0:
         print('post order traversal')
-    if False: # TODO
-        # TODO
-        return
-
+    if tree != None:
+        postorder_traversal(tree.left, level +1)
+        postorder_traversal(tree.right, level +1)
+        print(f' level = {level:^3d} : value {tree.value}')
 
 # NOT given to the students
 def search(root: Node, value: int) -> Node:
     # base cases
-    if False: # TODO
-        return None # TODO
-    elif False: # TODO
-        return None # TODO
+    if root == None or root.value == value:
+        return root
     # recursive step
     else:
-        if False: # TODO
-            return None # TODO
+        if value < root.value:
+            return search(root.left, value)
         else:
-            return None # TODO
+            return search(root.right, value)
 
 
 # NOT given to students
 def insert(root: Node, value: int) -> Node:
-    if False: # TODO
-        return None # TODO
+    if root == None:
+        return Node(value)
     else:
-        if False: # TODO
-            return None # TODO
-        elif False: # TODO
-            root.right = None # TODO
+        if value < root.value:
+            root.left = insert(root.left, value)
         else:
-            root.left = None # TODO
+            root.right = insert(root.right, value)
     return root
+
 
 
 # given to students
--- b_bfs.py
+++ b_bfs.py
@@ -28,7 +28,7 @@ search_space: list = [ start ]
 # step 3 while the search space is empty...
 while len(search_space) > 0:
     # remove a node from the search space (FILO)
-    current: Node = search_space.pop( None ) # TODO
+    current: Node = search_space.pop(  ) # TODO
 
     # if a None child is not being processed...
     if current != None:
@@ -36,6 +36,6 @@ while len(search_space) > 0:
         print(current.value)
 
         # add the left and right to the search space (FIFO)
-        search_space = None # TODO
+        search_space.append(current.right); search_space.append(current.left)
     # end if
 # end loop
